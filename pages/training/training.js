4d863bb9911c530cce15cb6116d24e97b39ba86d
kuber <kubertqiu@hotmail.com>
2018-12-12 17:26:28 +0800

    set Answering/Training/mine three pages

--- pages/training/training.js
+++ pages/training/training.js
@@ -1,66 +1,83 @@
-// pages/training/training.js
+//training.js
+//获取应用实例
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    hidden: true,
+    motto: 'Hello World',
+    userInfo: {},
+    feedList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onLoad: function () {
+    var that = this
+    console.log('onLoad')
+    wx.request({
+      url: 'http://elephstor.com/wxdev/feeds-init.json',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        that.setData({
+          feedList: res.data
+        })
+        try {
+          wx.setStorageSync('feeds', res.data)
+        } catch (e) { }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
+    console.info("被拉下了")
+  },
+  toPerson: function (e) {
+    console.log(e)
+    wx.navigateTo({
+      url: '../person/person?master=' + e.target.dataset.master
+    })
+  },
+  upper: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  lower: function () {
+    console.log("到底啦")
+    if (this.requestFlag === false) {
+      this.requestFlag = true
+      this.setData({
+        hidden: false
+      })
+      var that = this
+      setTimeout(that.getFeeds, 3000)
+    }
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  requestFlag: false,
+  getFeeds: function () {
+    var that = this
+    wx.request({
+      url: 'http://elephstor.com/wxdev/stamp1206.json',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        that.requestFlag = false
+        that.setData({
+          hidden: true
+        })
+        var feedsStrorage = wx.getStorageSync('feeds') || []
+        feedsStrorage = feedsStrorage.concat(res.data)
+        that.setData({
+          feedList: feedsStrorage
+        })
+        try {
+          wx.setStorageSync('feeds', feedsStrorage)
+        } catch (e) { }
+        console.log("同步成功啦")
+      }
+    })
   }
 })
